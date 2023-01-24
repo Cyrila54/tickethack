e2ffe26609329c03643ad7e92516c0f2e4db7893
Sophie <sobesancon@laposte.net>
2023-01-24 16:30:38 +0100

    modif frontend

---
+++ frontend/Home.js
@@ -0,0 +1,6 @@
+// <div id="notripresult">
+//                         <img src="/tickethack/frontend/images/notfound.png" />
+//                         <p>No trip found.</p>
+//                         <div id="okresult">
+//                             <p>${Departure} > ${Arrival} </p>
+//                             <button type="submit">Book</button> */}
