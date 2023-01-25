fafdff51430954e1840a071a095a5805a68c47b1
Cyrila54 <cyril.a54@icloud.com>
2023-01-25 12:20:33 +0100

    message

--- frontend/Cart.js
+++ frontend/Cart.js
@@ -4,9 +4,9 @@ function displayBookings(){
 
     fetch ('http://localhost:3000').then (response=>response.json()).then(data=>{
 
-    for (const element of data.resultFind){
+/*     for (const element of data.resultFind){
     document.querySelector('').innerHTML +=
-}
+} */
     })
 
 }
