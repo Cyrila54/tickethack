ce70122f1eac18c049659d081a07ae39a6cb4f05
Cyrila54 <cyril.a54@icloud.com>
2023-01-25 10:34:48 +0100

    message

--- frontend/Home.js
+++ frontend/Home.js
@@ -1,51 +1,79 @@
-document.getElementById('datePicker').valueAsDate = new Date(); //préremplir la date du jour par défaut
+/* OPERATION QUAND CLIC SUR SEARCH */
+document.getElementById("datePicker").valueAsDate = new Date(); //préremplir la date du jour par défaut
 
-document.querySelector('#searchbutton').addEventListener('click',
-    function () {
-        const selectedDate = document.querySelector('#datePicker').value
-        console.log(selectedDate)
-        const selectedDeparture = document.querySelector('#departure').value
-        const selectedArrival = document.querySelector('#arrival').value
-        fetch('http://localhost:3000/travels', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                departure: selectedDeparture,
-                arrival: selectedArrival,
-                date: selectedDate,
-            }),
-        })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data.trips)
-                if (data.trips.length > 0) {
-                    document.querySelector('#result').innerHTML =
-                        `<div id="trainresult">
-                            </div>`
-                    for (let i = 0; i < 20; i++) {
-                        document.querySelector('#trainresult').innerHTML += `
-                        <div class="train">
-                            <p>${data.trips[i].departure} > ${data.trips[i].arrival}</p>
-                            <p>${data.trips[i].date}</p>
-                            <p>${data.trips[i].price}</p>
-                            <button type="submit" id="bookbutton">Book</button>
-                        </div>`
-                    }
-                    const allButtons = document.querySelectorAll(`#bookbutton`);
-                    for (let j = 0; j < allButtons.length; j++) {
-                        allButtons[j].addEventListener('click', function () {
-                            console.log(data.trips[j]) // Remplacer par la bonne route pour enregistrer le choix en BDD
-                        })
-                    }
-                } else {
-                    document.querySelector('#result').innerHTML = `
-                    <div id="noresult">
-                        <img class="imageresult" src="/tickethack/frontend/images/notfound.png" />
-                        <hr>
-                        <p class="textresult">No trip found.</p>
-                    </div>`;
-                }
+document.querySelector("#searchbutton").addEventListener("click", () => {
+  fetch("http://localhost:3000/travels/find", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      departure: document.querySelector("#departure").value,
+      arrival: document.querySelector("#arrival").value,
+      date: document.querySelector("#datePicker").value,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data.result);
+      if (data.result.length == 0) {
+        console.log("zero");
+        document.querySelector("#result").innerHTML = `
+        <div id="noresult">
+            <img class="imageresult" src="/tickethack/frontend/images/notfound.png" />
+            <hr>
+            <p class="textresult">No trip found.</p>
+        </div>`;
+      } else if (data.result.length > 0) {
+        
 
+        for (const element of data.result) {
+          let newDate = Date.parse(element.date);
+
+          let hours = new Date(newDate).getHours();
+          let minutes = new Date(newDate).getMinutes();
+          if (hours < 10) {
+            hours = "0" + hours;
+          }
+          if (minutes < 10) {
+            minutes = "0" + minutes;
+          }
+          let formattedTime = hours + ":" + minutes;
+
+          document.querySelector("#result").innerHTML += `
+              <div class="train">
+        <div class="trajet">
+          <span><span id="depart">${element.departure}</span> > <span id="arrivee">${element.arrival}</span><span id="formatDate"> ${formattedTime}</span> <span id="price">${element.price}</span>€</span>
+          <button class="bookbutton">Book</button>
+        </div>
+      </div>
+              `;
+        }
+      }
+      /* AJOUT A 'BOOKINGS' LE CHOIX SELECTIONNE */
+
+      for (
+        let i = 0;
+        i < document.querySelectorAll(".bookbutton").length;
+        i++
+      ) {
+        document
+          .querySelectorAll(".bookbutton")
+          [i].addEventListener("click", () => {
+            fetch("http://localhost:3000/bookings", {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify({
+                departure: document.querySelectorAll("#depart")[i].textContent,
+                arrival: document.querySelectorAll("#arrivee")[i].textContent,
+                date: document.querySelectorAll("#formatDate")[i].textContent,
+                price: document.querySelectorAll("#price")[i].textContent,
+              }),
             })
-    })
-
+              .then((response) => response.json)
+              .then((data) => {
+                console.log(data.Booking);
+                console.log(data);
+              });
+          });
+      }
+    });
+});
